a1387a063456bd791a6cdefe2553251316e7e684
mc_fdc <Masato.04.11.2007@gmail.com>
2022-07-09 18:05:33 +0900

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 import { Client } from "discord.js"
 
 
-client = new Client(
+client = new Client({
   intents: ['GUILDS', 'GUILD_MEMBERS', 'GUILD_MESSAGES']
-)
+})
 
 client.once("ready", () => {
   console.log("Ready")
