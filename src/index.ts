d0e46eaaab5757f5ab6e14c7aca988d340a0719a
mc_fdc <Masato.04.11.2007@gmail.com>
2022-07-09 18:37:54 +0900

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,15 @@
 import { Client } from "discord.js"
 import config from "./config.json"
+import commands from "./commands.json"
 
 
 const client = new Client({
   intents: ['GUILDS', 'GUILD_MEMBERS', 'GUILD_MESSAGES'],
 })
 
-client.once("ready", () => {
+client.once("ready", async () => {
   console.log("Ready")
+  await client.application.commands.set(commands)
 })
 
 client.login(config.token)
