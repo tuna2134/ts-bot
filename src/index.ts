77edc117ba8cbdcbfdb5ecf2494f46c64a74032c
mc_fdc <Masato.04.11.2007@gmail.com>
2022-07-09 18:02:55 +0900

    Update index.ts

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import { Client } from "discord.js"
+
+
+client = new Client(
+  intents: ['GUILDS', 'GUILD_MEMBERS', 'GUILD_MESSAGES']
+)
+
+client.once("ready", () => {
+  console.log("Ready")
+})
+
+client.login()
